4d928200e5e8f3462af9fd30af8c2f4f00d0ee05
Mr-BotNoob <44126876+Mr-BotNoob@users.noreply.github.com>
2021-03-14 12:17:44 +0700

    Sign In, Sign Up, Forgot and Slider

---
+++ app/src/main/java/com/aguna/app/presentation/introslide.kt
@@ -0,0 +1,7 @@
+package com.aguna.app.presentation
+
+class introslide (
+    val title: String,
+    val description: String,
+    val icon: Int
+)
---
+++ app/src/main/java/com/aguna/app/presentation/introslider_adapter.kt
@@ -0,0 +1,46 @@
+package com.aguna.app.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.aguna.app.R
+
+class introslider_adapter (private val introslide: List<introslide>)
+    : RecyclerView.Adapter<introslider_adapter.IntroSlideViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IntroSlideViewHolder {
+        return IntroSlideViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.splash_slider_container,
+                parent,
+                false
+            )
+
+        )
+    }
+
+    override fun onBindViewHolder(holder: IntroSlideViewHolder, position: Int) {
+        holder.bind(introslide[position])
+
+    }
+
+    override fun getItemCount(): Int {
+        return introslide.size
+
+    }
+
+    inner class IntroSlideViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        private val textTitle = view.findViewById<TextView>(R.id.textTitle)
+        private val textDescription = view.findViewById<TextView>(R.id.textDescription)
+        private val imageIcon = view.findViewById<ImageView>(R.id.imageslideIcon)
+
+        fun bind(introslide: introslide){
+            textTitle.text = introslide.title
+            textDescription.text = introslide.description
+            imageIcon.setImageResource(introslide.icon)
+        }
+    }
+}
