4d755d08329af992cb5fad8d551097f44ace0307
Mr-BotNoob <44126876+Mr-BotNoob@users.noreply.github.com>
2021-04-30 21:54:34 +0700

    Photo Upload

--- app/src/main/java/com/aguna/app/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/aguna/app/presentation/profile/ProfileFragment.kt
@@ -1,7 +1,12 @@
 package com.aguna.app.presentation.profile
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,14 +17,22 @@ import androidx.navigation.fragment.findNavController
 import com.aguna.app.R
 import com.aguna.app.presentation.MainActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 import kotlinx.android.synthetic.main.fragment_sign_in.*
+import java.io.ByteArrayOutputStream
 
+@Suppress("DEPRECATION")
 class ProfileFragment : Fragment() {
 
-    private lateinit var auth  : FirebaseAuth
+    companion object{
+        const val REQUEST_CAMERA = 100
+    }
 
+    private lateinit var auth  : FirebaseAuth
+    private lateinit var imageUri: Uri
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
@@ -37,11 +50,12 @@ class ProfileFragment : Fragment() {
      return view
     }
 
-//    Logout
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         auth = FirebaseAuth.getInstance()
 
+        //    Logout
         textView16.setOnClickListener{
             auth.signOut()
             val i = Intent(activity,
@@ -50,5 +64,49 @@ class ProfileFragment : Fragment() {
                     Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(i)
         }
+
+        imageView2.setOnClickListener {
+            intentcamera()
+        }
+
     }
+
+    private fun intentcamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+            activity?.packageManager?.let {
+                intent.resolveActivity(it).also{
+                    startActivityForResult(intent, REQUEST_CAMERA)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CAMERA && resultCode == RESULT_OK){
+            val imgBitmap = data?.extras?.get("data") as Bitmap
+            uploadImage(imgBitmap)
+        }
+    }
+
+    private fun uploadImage(imgBitmap: Bitmap) {
+        val baos = ByteArrayOutputStream()
+        val ref = FirebaseStorage.getInstance().reference.child("avatar/${FirebaseAuth.getInstance().currentUser?.uid}")
+
+        imgBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val image = baos.toByteArray()
+
+        ref.putBytes(image)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    ref.downloadUrl.addOnCompleteListener {
+                        it.result?.let {
+                            imageUri= it
+                            imageView2.setImageBitmap(imgBitmap)
+                        }
+                    }
+                }
+            }
+    }
+
 }
